d12fbd5a90730b5e21694b7bb9b1008852ce87d0
Ken <kennethlieu@gmail.com>
2024-03-13 19:26:20 -0600

    Bot can connect and reply in chat

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ from dotenv import dotenv_values
 
 TOKEN = dotenv_values(".env")["TOKEN"]
 
+# Intents are the events that are listened for
+intents = discord.Intents.default()
+intents.message_content = True
 
+client = discord.Client(intents=intents)
+
+
+# @ Symbol means decorator, in this case, the client.event decorator function
+# Essentially, registers an event for the client object to listen to.
+# check the github for code, or setattr to understand.
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("HELLO")
+
+
+client.run(TOKEN)
