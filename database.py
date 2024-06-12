b31dd22f40513274a2bb9f26bb38d36fe6498d13
Ken <kennethlieu@gmail.com>
2024-06-12 15:10:23 -0600

    basic database queries / updates

---
+++ database.py
@@ -0,0 +1,50 @@
+from sqlite3 import Cursor, Connection, connect
+from os.path import exists
+
+connection = Connection
+cursor = Cursor
+
+def init_setup():
+    global connection, cursor
+
+    db_exists = exists("./chips.db")
+    connection = connect("chips.db")
+    cursor = connection.cursor()
+
+    if not db_exists:
+        cursor.execute("""CREATE TABLE chips(
+                       id INTEGER PRIMARY KEY, 
+                       value INTEGER)""")
+
+def register(id: int):
+    global connection, cursor
+    cursor = cursor.execute(f"""
+    SELECT id
+    FROM chips
+    WHERE id = {id}""")
+
+    if cursor.fetchone() == None:
+        cursor.execute(f"""
+            INSERT INTO chips (id, value)
+            VALUES ({id}, 1000)""")
+
+        connection.commit()
+
+def update(id: int, difference: int):
+    global connection, cursor
+
+    original_val = cursor.execute(f"""
+                    SELECT value
+                    FROM chips
+                    WHERE id = {id}""").fetchone()[0]
+
+    cursor.execute(f"""
+                    UPDATE chips
+                    SET value = {original_val + difference}
+                    WHERE id = {id}""")
+
+    connection.commit()
+
+
+if __name__ == "__main__":
+    init_setup()
